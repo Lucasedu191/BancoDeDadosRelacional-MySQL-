3f1d9267ab134cb7bc2cc8c29651992e11ced026
Lucas <lucasatletico@hotmail.com>
2020-05-12 19:53:52 -0300

    adicionando primeiras tabelas Estado

---
+++ banco-relacional/inserirEstados.sql
@@ -0,0 +1,8 @@
+insert  into estados (nome, sigla, regiao, populacao)
+VALUE ('Acre', 'AC', 'Norte', 0.83);
+
+insert into estados (nome, sigla, regiao, populacao)
+value 
+('Alagoas', 'AL', 'Nordeste', 3.38),
+('Amapá', 'AP', 'Norte', 0.8),
+('Amazonas', 'AM', 'Norte', 4.6);
